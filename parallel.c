f4c8f9694a897810d2c7f25316eaac1893cbe633
VictorColome <victorcolomec@gmail.com>
2018-05-13 16:36:45 +0200

    :/

--- parallel.c
+++ parallel.c
@@ -13,137 +13,134 @@ float distancia[N][N+1];
 
 int cami[N],bo[N];
 
-int main(int na, char* arg[]){
+int main(int na, char* arg[])
+{
     int nn,i,j,primer,actual,index,end;
     int origin = 0;
     float dmin,dist,millor = GRAN;
-    float aux = -1;
-
-    assert(na == 2);
+    float aux = GRAN;
 
     int el_meu_rank; // rank
     int p; // num_processos
-    int millor_rank = -1; // millor rank
     MPI_Status estat; // estat de la recepció
+    
+    assert(na == 2);
 
     MPI_Init(&na, &arg); // Init MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &el_meu_rank); // Obtenir rank del proces
     MPI_Comm_size(MPI_COMM_WORLD, &p); // Obtenir num_processos
 
+    float millors[p];
+    int millor_rank = el_meu_rank; // millor rank
     int sizes[p]; 
 
     nn = atoi(arg[1]);
     assert(nn<=N);
     srand(1);
-
-    if(el_meu_rank == 0)
+    
+    if (el_meu_rank == 0)
     {
         printf("Dimensio %s\n",arg[1]);
-
         for(i=0; i<nn; i++) X[i]=rand()%(nn*10);
         for(i=0; i<nn; i++) Y[i]=rand()%(nn*10);
-
-        int buff_size = nn / p;
-        int extra_size = nn % p;
-
-        for (i = 0; i < p; ++i)
-            sizes[i] = buff_size;
-        i = 0;
-        while(extra_size > 0){
-            sizes[i++] += 1;
-            extra_size--;
-        }
-        for (i = 1; i < p; ++i)
-            sizes[i] += sizes[i-1];
     }
-
-    MPI_Bcast(sizes, p, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(X, nn, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(Y, nn, MPI_INT, 0, MPI_COMM_WORLD);
-
-    for(i=0; i<nn; i++) distancia[i][i]=0;
-    for(i=0; i<nn; i++)  
-        for(j=i+1; j<nn; j++) 
-            distancia[i][j]= distancia[j][i] = sqrt(pow(X[i]-X[j],2) + pow(Y[i]-Y[j],2));
     
+    int buff_size = nn / p;
+    int extra_size = nn % p;
+
+    for (i = 0; i < p; ++i)
+        sizes[i] = buff_size;
+    i = 0;
+    while (extra_size > 0)
+    {
+        sizes[i++] += 1;
+        extra_size--;
+    }
+    for (i = 1; i < p; ++i)
+        sizes[i] += sizes[i-1];
+
     if (el_meu_rank != 0)
         origin = sizes[el_meu_rank - 1];
 
     end = sizes[el_meu_rank];
 
-    int size = origin + sizes[0];
+    MPI_Bcast(X, nn, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(Y, nn, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    // for(i=0; i<nn; i++) distancia[i][i]=0;
+    for(i=0; i<nn; i++)  
+        for(j=i+1; j<nn; j++) 
+            distancia[i][j]= distancia[j][i] = sqrt(pow(X[i]-X[j],2) + pow(Y[i]-Y[j],2));
     
     // TOTs amb Greedy
-    for (primer=origin; primer<size; primer++) 
+    for (primer = origin; primer < end; primer++) 
     {
-        if (primer < end)
+        dist = 0;
+        for(i = 0; i < nn; i++) cami[i] = -1;
+        cami[primer] = 0;
+        actual = primer;
+        for (i = 1; i < nn; i++) 
         {
-            dist = 0;
-            for(i = 0; i < nn; i++) cami[i] = -1;
-            cami[primer] = 0;
-            actual = primer;
-            for (i = 1; i < nn; i++) 
+            dmin = GRAN;
+            index = 0; // redundant
+            for (j = 0;j < nn; j++) 
             {
-                dmin = GRAN;
-                index = 0; // redundant
-                for (j = 0;j < nn; j++) 
+                if (cami[j] == -1 && actual != j && distancia[actual][j] < dmin) 
                 {
-                    if (cami[j] == -1 && actual != j && distancia[actual][j] < dmin) 
-                    {
-                        dmin = distancia[actual][j];
-                        index= j;
-                    }
+                    dmin = distancia[actual][j];
+                    index= j;
                 }
-                actual = index;
-                cami[actual] = i;
-                dist += dmin;
-                // PODA
-                if (dist >= millor) { dist = 0; break;}
             }
+            actual = index;
+            cami[actual] = i;
+            dist += dmin;
+            // PODA
+            if (dist >= millor) { dist = 0; break;}
         }
-        for (j = 0; j < p; j++) 
+        if (dist) 
         {
-            aux = -1;
-            if (el_meu_rank == j)
+            dmin = distancia[actual][primer];
+            dist += dmin;
+            if (dist < millor) 
             {
-                if (dist) 
-                {
-                    dmin = distancia[actual][primer];
-                    dist += dmin;
-                    if (dist < millor) 
-                    {
-                        for(i = 0; i < nn; i++) bo[cami[i]] = i;
-                        millor = dist;
-                        aux = millor;
-                    } 
-                    distancia[primer][nn] = dist;  // per guardar alternatives
-                }
-            }
-            MPI_Bcast(&aux, 1, MPI_FLOAT, j, MPI_COMM_WORLD);
-            if (aux <= millor && aux != -1) 
-            {
-                millor = aux;
-                millor_rank = j;
+                for(i = 0; i < nn; i++) bo[cami[i]] = i;
+                millor = dist;
             } 
-        }
+            distancia[primer][nn] = dist;  // per guardar alternatives
+        }  
     }
 
-    if (el_meu_rank != 0 && el_meu_rank == millor_rank) 
+    MPI_Gather(&millor, 1, MPI_FLOAT, millors, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    if (el_meu_rank == 0)
+    {
+       for(i = 0; i < p; i++)
+       {
+            if (millors[i] < aux)
+            {
+                millor_rank = i;
+                aux = millors[i];
+            }
+       }
+    }
+    
+    MPI_Bcast(&millor_rank, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (el_meu_rank == millor_rank && el_meu_rank != 0)
     {
         MPI_Send(bo, nn, MPI_INT, 0, 0, MPI_COMM_WORLD);
         MPI_Send(&distancia[bo[0]][nn], 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
     }
 
-    if(el_meu_rank == 0)
+    if (el_meu_rank == 0)
     {
-        if (el_meu_rank != millor_rank) 
+        millor = millors[millor_rank];
+        if (millor_rank != 0)
         {
             MPI_Recv(bo, nn, MPI_INT, millor_rank, 0, MPI_COMM_WORLD, &estat);
             MPI_Recv(&distancia[bo[0]][nn], 1, MPI_FLOAT, millor_rank, 0, MPI_COMM_WORLD, &estat);
         }
-
         printf("Solucio :\n");
-        for(i=0; i<nn; i++)
+        for(i = 0; i < nn; i++)
             printf("%d\n",bo[i]);
         printf("Distancia %g == %g\n", millor, distancia[bo[0]][nn]);
     }
